8a633fa5d265029960ab399e5bd33b10e5674256
luiz <luizfernandovieira_cf@hotmail.com>
2016-07-17 10:19:39 -0300

    add geometry_helper com funcoes que manipular vectors e rects

---
+++ WhatIsLove/geometry_helper.lua
@@ -0,0 +1,21 @@
+function projectionX(module, angle)
+  angle = angle * math.pi / 180
+  return math.cos(angle) * module
+end
+
+function projectionY(module, angle)
+  angle = angle * math.pi / 180
+  return math.sin(angle) * module
+end
+
+function distance(v1, v2)
+  return math.sqrt(math.pow(v1.x-v1.x, 2) + math.pow(v2.y-v2.y, 2))
+end
+
+function lineInclination(v1, v2)
+  angle = math.atan((v2.y-v1.y) / (v2.x-v1.x)) * 180 / math.pi
+  if v1.x > v2.x then
+    return angle + 180
+  end
+  return angle
+end
